f930a0e55671ddc790aaceec8387fb01568636e4
Dallana Bastidas <cindydallana2822@gmail.com>
2024-12-10 18:24:24 -0500

    encuesta

---
+++ encuesta/index.js
@@ -0,0 +1,48 @@
+document.addEventListener("DOMContentLoaded", () => {
+  // carrusel
+  const splide = new Splide("#encuesta", {
+    type: "slide", // Tipo de carrusel
+    perPage: 1, // Mostrar una diapositiva a la vez
+    perMove: 1, // Mover una diapositiva por vez
+    pagination: true, // Mostrar paginación
+    arrows: true, // Mostrar flechas de navegación
+  }).mount(); // Montar el carrusel
+
+  const form = document.getElementById("encuesta");
+
+  const formDataArray = [];
+
+  const paginationDots = document.querySelectorAll(".splide__pagination__page");
+
+  const fields = document.querySelectorAll(
+    'input[type="text"],input[type="radio"],input[type="checkbox"], select, textarea, input[type="date"]'
+  );
+
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const name = document.getElementById("name").value;
+    const gender = document.querySelector('input[type="radio"]:checked')?.value;
+    const ageGroup = document.getElementById("ageGroup").value;
+    const comments = document.getElementById("comments").value;
+    const date = document.getElementById("date").value;
+    const interests = [];
+    document
+      .querySelectorAll('input[name="interests"]:checked')
+      .forEach((checkbox) => {
+        interests.push(checkbox.value);
+      });
+
+    const formData = {
+      name,
+      gender,
+      ageGroup,
+      comments,
+      interests,
+      date,
+    };
+
+    formDataArray.push(formData);
+
+    console.log(formData);
+  });
+});
